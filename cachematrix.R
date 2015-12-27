c9dd784c63215bf64b2af7b7f845b86b5cf81806
Nelly Anbeek <Nelly@BMA.local>
2015-12-27 15:26:32 +0100

    20151227 Some minor adjustments

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-## makeCacheMatrix creates a "special" matrix, that makes it possible to store it's inverse
+## makeCacheMatrix creates a "special" matrix, that makes it possible to store it's inverse in cache
 ## At creation the inverse matrix (sol) is initialized.
 ## Functions:
 ## - set: Stores the actual matrix
@@ -7,33 +7,35 @@
 ## - getsolution: Returns the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-    sol <- matrix(ncol = ncol(x))
+    sol <- NULL
     set <- function(y) {
         x <<- y
-        sol <<- matrix(ncol = ncol(y))
+        sol <<- NULL
     }
     get <- function() x
     setsolution <- function(solution) sol <<- solution
     getsolution <- function() sol
     list(set = set, get = get,
          setsolution = setsolution,
-         getsolution = setsolution)
+         getsolution = getsolution)
 
 }
 
 ## cacheSolve returns the inverse of a matrix
 ## - If the inverse has been calculated before, and stored in the cache it just
 ##   returns the inverse from cache
-## - If the inverse has not been stored yet, in calculates the inverse matrix using
+## - If the inverse has not been stored yet, it calculates the inverse matrix using
 ##   the funtion "solve"
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     sol <- x$getsolution()
+    ## If sulution is stored get it from the cache
     if(!is.null(sol)) {
         message("getting cached data")
         return(sol)
     }
+    ## Otherwise calculate the inverse matrix and store it in cache
     data <- x$get()
     sol <- solve(data, ...)
     x$setsolution(sol)
